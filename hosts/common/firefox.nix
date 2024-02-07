58c6b1af275be1766513df30353e9a26c83aa5b6
lordkekz <lordkekz@lkekz.de>
2024-02-08 00:06:30 +0100

    nixos: Firefox changes (extension, dark mode, refactoring)

--- hosts/common/firefox.nix
+++ hosts/common/firefox.nix
@@ -7,16 +7,10 @@
   pkgs,
   ...
 }: let
-  lock-false = {
-    Value = false;
-    Status = "locked";
-  };
-  lock-true = {
-    Value = true;
-    Status = "locked";
-  };
-  lock-empty-string = {
-    Value = "";
+  lock-false = lock-value false;
+  lock-true = lock-value true;
+  lock-value = Value: {
+    inherit Value;
     Status = "locked";
   };
   mkExt = addon-name: {
@@ -74,6 +68,7 @@ in {
         "{b86e4813-687a-43e6-ab65-0bde4ab75758}" = mkExt "localcdn-fork-of-decentraleyes";
 
         # Youtube
+        "enhancerforyoutube@maximerf.addons.mozilla.org" = mkExt "enhancer-for-youtube";
         "{dccd64f0-f884-496c-af8b-1a66330e1a67}" = mkExt "youtube-audio_only";
         "{b9db16a4-6edc-47ec-a1f4-b86292ed211d}" = mkExt "video-downloadhelper";
         "sponsorBlocker@ajay.app" = mkExt "sponsorblock";
@@ -115,15 +110,13 @@ in {
         "browser.newtabpage.activity-stream.system.showSponsored" = lock-false;
         "browser.newtabpage.activity-stream.showSponsoredTopSites" = lock-false;
         "browser.download.autohideButton" = lock-false;
-        "browser.firefox-view.feature-tour" = {
-          Value = ''{"message":"FIREFOX_VIEW_FEATURE_TOUR","screen":"","complete":true}'';
-          Status = "default";
-        };
+        "browser.firefox-view.feature-tour" = lock-value ''{"message":"FIREFOX_VIEW_FEATURE_TOUR","screen":"","complete":true}'';
+
         "dom.security.https_only_mode" = lock-true;
-        "widget.use-xdg-desktop-portal.file-picker" = {
-          Value = 1;
-          Status = "lock";
-        };
+        "widget.use-xdg-desktop-portal.file-picker" = lock-value 1;
+
+        "browser.uiCustomization.state" = lock-value ''{"placements":{"widget-overflow-fixed-list":["characterencoding-button"],"unified-extensions-area":["sponsorblocker_ajay_app-browser-action","dearrow_ajay_app-browser-action","jid1-mnnxcxisbpnsxq_jetpack-browser-action","_b86e4813-687a-43e6-ab65-0bde4ab75758_-browser-action","_531906d3-e22f-4a6c-a102-8057b88a1a63_-browser-action","_580efa7d-66f9-474d-857a-8e2afc6b1181_-browser-action","_c5f935cf-9b17-4b85-bed8-9277861b4116_-browser-action","_b9db16a4-6edc-47ec-a1f4-b86292ed211d_-browser-action","enhancerforyoutube_maximerf_addons_mozilla_org-browser-action"],"nav-bar":["back-button","forward-button","stop-reload-button","urlbar-container","bookmarks-menu-button","downloads-button","history-panelmenu","jetpack-extension_dashlane_com-browser-action","ublock0_raymondhill_net-browser-action","umatrix_raymondhill_net-browser-action","_dccd64f0-f884-496c-af8b-1a66330e1a67_-browser-action","extension_one-tab_com-browser-action","unified-extensions-button","plasma-browser-integration_kde_org-browser-action"],"toolbar-menubar":["menubar-items"],"TabsToolbar":["tabbrowser-tabs","new-tab-button","alltabs-button","firefox-view-button"],"PersonalToolbar":["import-button","personal-bookmarks"]},"seen":["save-to-pocket-button","developer-button","jetpack-extension_dashlane_com-browser-action","_b86e4813-687a-43e6-ab65-0bde4ab75758_-browser-action","extension_one-tab_com-browser-action","ublock0_raymondhill_net-browser-action","dearrow_ajay_app-browser-action","_531906d3-e22f-4a6c-a102-8057b88a1a63_-browser-action","_580efa7d-66f9-474d-857a-8e2afc6b1181_-browser-action","_c5f935cf-9b17-4b85-bed8-9277861b4116_-browser-action","umatrix_raymondhill_net-browser-action","sponsorblocker_ajay_app-browser-action","jid1-mnnxcxisbpnsxq_jetpack-browser-action","_dccd64f0-f884-496c-af8b-1a66330e1a67_-browser-action","_b9db16a4-6edc-47ec-a1f4-b86292ed211d_-browser-action","plasma-browser-integration_kde_org-browser-action","enhancerforyoutube_maximerf_addons_mozilla_org-browser-action"],"dirtyAreaCache":["nav-bar","PersonalToolbar","toolbar-menubar","TabsToolbar","unified-extensions-area","widget-overflow-fixed-list"],"currentVersion":20,"newElementCount":4}'';
+        "layout.css.prefers-color-scheme.content-override" = lock-value 0; # Set website appearance to dark mode.
       };
     };
   };
