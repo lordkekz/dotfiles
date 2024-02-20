7f9e78791cee2701a8b877d45f3f269dd247fb23
lordkekz <lordkekz@lkekz.de>
2024-02-21 00:42:37 +0100

    hpreiser@Term: Add some configuration to nixvim

--- home/hpreiser/Term/nixvim.nix
+++ home/hpreiser/Term/nixvim.nix
@@ -17,11 +17,26 @@ args @ {
   programs.nixvim = {
     enable = true;
     viAlias = true;
+
+    options = {
+      number = true; # Show line numbers
+      relativenumber = true; # Show relative line numbers
+
+      shiftwidth = 2; # Tab width should be 2
+    };
+
+    # LSP
     plugins.lsp.enable = true;
     plugins.lsp.servers = {
       nixd.enable = true;
       #nushell.enable = true; # Not yet available on nixos-23.11; mixing nixvim unstable and nixpkgs stable wreaks havoc.
       java-language-server.enable = true;
     };
+
+    # Misc plugins
+    plugins.which-key.enable = true;
+    extraPlugins = with pkgs.vimPlugins; [
+      vim-numbertoggle
+    ];
   };
 }
