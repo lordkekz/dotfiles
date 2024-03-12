cc21f6427234eff0aa1d75ea12ae72f50fc095de
lordkekz <lordkekz@lkekz.de>
2024-03-13 00:26:51 +0100

    hpreiser@Term: Add fancy git log alias

--- home/hpreiser/Term/git.nix
+++ home/hpreiser/Term/git.nix
@@ -33,6 +33,9 @@ in {
     extraConfig = {
       init.defaultBranch = "main";
     };
+    aliases = {
+      lg = ''log --pretty=format:"%C(cyan)@%an%Creset%C(dim)[%Creset%C(green bold)%G?%Creset%C(dim)]%Creset %C(yellow)%h%Creset %C(bold)%s%Creset" --all --graph'';
+    };
     attributes = [
       # global .gitattributes
     ];
