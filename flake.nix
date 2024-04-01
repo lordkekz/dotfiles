e6f628cf3ac131025023d7900c9792c93621907c
lordkekz <lordkekz@lkekz.de>
2024-04-01 23:59:00 +0200

    dot2: sort flake inputs

--- flake.nix
+++ flake.nix
@@ -2,49 +2,17 @@
   description = "A cookie jar full of flakes.";
 
   inputs = {
-    # Nixpkgs
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.11";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs.follows = "nixpkgs-stable";
+    ## PURE-NIX UTILITIES ##
 
-    # Home manager
-    home-manager.url = "github:nix-community/home-manager/release-23.11";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
-    # Plasma manager
-    plasma-manager.url = "github:pjones/plasma-manager";
-    plasma-manager.inputs.nixpkgs.follows = "nixpkgs";
-    plasma-manager.inputs.home-manager.follows = "home-manager";
-
-    # Use sops-nix for secret management
-    sops-nix.url = "github:Mic92/sops-nix";
-    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
-    sops-nix.inputs.nixpkgs-stable.follows = "nixpkgs-stable";
-
-    # Hardware configs for known systems.
-    hardware.url = "github:nixos/nixos-hardware";
-
-    # Mirror of VSCode marketplace and Open VSX registry.
-    nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
-    nix-vscode-extensions.inputs.nixpkgs.follows = "nixpkgs";
-
-    # NixVim distro for neovim
-    nixvim.url = "github:nix-community/nixvim/main";
-    #nixvim.url = "github:lordkekz/nixvim/main";
-    nixvim.inputs.nixpkgs.follows = "nixpkgs-unstable";
-    # Doesn't work: nixvim.inputs.pre-commit-hooks.inputs.nixpkgs-stable.follows = "nixpkgs-stable";
-    # see: https://github.com/msteen/nix-flake-override
-
-    # NixOS-WSL
-    NixOS-WSL.url = "github:nix-community/NixOS-WSL";
-    NixOS-WSL.inputs.nixpkgs.follows = "nixpkgs";
-
-    # A nixpkgs downstream which only contains the lib
-    nixpkgs-lib.url = "github:nix-community/nixpkgs.lib";
+    # Flake utils for stripping some boilerplate
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-utils.inputs.systems.follows = "systems";
+    flake-utils-plus.url = "github:gytis-ivaskevicius/flake-utils-plus/v1.3.0";
+    flake-utils-plus.inputs.flake-utils.follows = "flake-utils";
 
     # Haumea for directory-defined attrset loading
     haumea.url = "github:nix-community/haumea/v0.2.2";
-    haumea.inputs.nixpkgs.follows = "nixpkgs-lib";
+    haumea.inputs.nixpkgs.follows = "nixpkgs";
 
     # My personal data which is *not* technically secret but I don't want to show in this repo.
     personal-data.url = "github:lordkekz/personal-data";
@@ -55,26 +23,58 @@
     # The list of supported systems.
     systems.url = "github:nix-systems/default-linux";
 
-    # Flake utils for stripping some boilerplate
-    flake-utils-plus.url = "github:gytis-ivaskevicius/flake-utils-plus/v1.3.0";
-    flake-utils-plus.inputs.flake-utils.follows = "flake-utils";
-    flake-utils.url = "github:numtide/flake-utils";
-    flake-utils.inputs.systems.follows = "systems";
+    ## PACKAGES, CONFIGURATION AND APPLICATIONS ##
 
-    # Shameless plug: looking for a way to nixify your themes and make
-    # everything match nicely? Try nix-colors!
-    # nix-colors.url = "github:misterio77/nix-colors";
+    # Hardware configs for known systems.
+    hardware.url = "github:nixos/nixos-hardware";
+
+    # Home manager
+    home-manager.url = "github:nix-community/home-manager/release-23.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    # Nixpkgs
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.follows = "nixpkgs-stable";
+
+    # A nixpkgs downstream which only contains the lib
+    #nixpkgs-lib.url = "github:nix-community/nixpkgs.lib";
+
+    # NixVim distro for neovim
+    nixvim.url = "github:nix-community/nixvim/main";
+    #nixvim.url = "github:lordkekz/nixvim/main";
+    nixvim.inputs.nixpkgs.follows = "nixpkgs-unstable";
+    # Doesn't work: nixvim.inputs.pre-commit-hooks.inputs.nixpkgs-stable.follows = "nixpkgs-stable";
+    # see: https://github.com/msteen/nix-flake-override
+
+    # Mirror of VSCode marketplace and Open VSX registry.
+    nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
+    nix-vscode-extensions.inputs.nixpkgs.follows = "nixpkgs";
+
+    # NixOS-WSL
+    NixOS-WSL.url = "github:nix-community/NixOS-WSL";
+    NixOS-WSL.inputs.nixpkgs.follows = "nixpkgs";
+
+    # Plasma manager
+    plasma-manager.url = "github:pjones/plasma-manager";
+    plasma-manager.inputs.nixpkgs.follows = "nixpkgs";
+    plasma-manager.inputs.home-manager.follows = "home-manager";
+
+    # Use sops-nix for secret management
+    sops-nix.url = "github:Mic92/sops-nix";
+    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
+    sops-nix.inputs.nixpkgs-stable.follows = "nixpkgs-stable";
   };
 
   outputs = inputs @ {
-    self,
-    nixpkgs,
-    nixpkgs-stable,
-    nixpkgs-unstable,
-    systems,
     flake-utils-plus,
     haumea,
     home-manager,
+    nixpkgs,
+    nixpkgs-stable,
+    nixpkgs-unstable,
+    self,
+    systems,
     ...
   }: let
     inherit (self) outputs;
@@ -150,7 +150,8 @@
               pkgs-unstable = channels.nixpkgs-unstable;
             };
             modules = [homeProfile];
-          }) homeProfiles;
+          })
+        homeProfiles;
 
         # Output channels for easier debugging in nix repl
         inherit channels;
