2d55b3ee154c5b350de04fc165e6b5a15cfb8665
lordkekz <lordkekz@lkekz.de>
2024-04-08 15:41:43 +0200

    dot2: add hardwareProfiles/framework-laptop-2022

--- dot2/hardwareProfiles/physical/physical.nix
+++ dot2/hardwareProfiles/physical/physical.nix
@@ -7,14 +7,6 @@
   modulesPath,
   ...
 }: {
-  imports = [
-    # Get the defaults from nixos-hardware
-    inputs.hardware.nixosModules.framework-12th-gen-intel
-
-    # Default imports
-    (modulesPath + "/installer/scan/not-detected.nix")
-  ];
-
   # Bootloader.
   boot.loader = {
     # Use systemd-boot
@@ -31,19 +23,9 @@
     timeout = 1;
   };
 
-  # See: https://wiki.archlinux.org/title/Framework_Laptop_13#Lowering_fan_noise
-  services.thermald.enable = true;
-
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-
-  boot.initrd.availableKernelModules = ["xhci_pci" "thunderbolt" "nvme" "uas" "usb_storage" "usbhid" "sd_mod"];
-  boot.initrd.kernelModules = [];
-  boot.kernelModules = ["kvm-intel"];
-  boot.extraModulePackages = [];
-
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
