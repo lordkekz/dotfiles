e6e06cd73ccede75c1e798f9783c02a8cb0403c1
lordkekz <lordkekz@lkekz.de>
2024-04-08 15:41:44 +0200

    dot2: nixosProfiles/common: remove nixpkgs config

--- dot2/nixosProfiles/common/common.nix
+++ dot2/nixosProfiles/common/common.nix
@@ -9,38 +9,12 @@
 }: {
   ### BASIC NIX and NIXPKGS STUFF ###
 
-  nixpkgs = {
-    # You can add overlays here
-    overlays = [
-      # Add overlays your own flake exports (from overlays and pkgs dir):
-      #outputs.overlays.additions
-      #outputs.overlays.modifications
-      #outputs.overlays.unstable-packages
-
-      # You can also add overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
-
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
-    ];
-    # Configure your nixpkgs instance
-    # config = {
-      # Disable if you don't want unfree packages
-      #allowUnfree = true;
-    #};
-
-    hostPlatform = "x86_64-linux";
-  };
-
   # This will add each flake input as a registry
   # To make nix3 commands consistent with your flake
   nix.registry =
     (lib.mapAttrs (_: flake: {inherit flake;}))
     ((lib.filterAttrs (_: lib.isType "flake")) inputs);
+  # flake-utils-plus: nix.generateRegistryFromInputs = true;
 
   # This will additionally add your inputs to the system's legacy channels
   # Making legacy nix commands consistent as well, awesome!
@@ -51,6 +25,7 @@
       value.source = value.flake;
     })
     config.nix.registry;
+  # flake-utils-plus: nix.generateNixPathFromInputs = true;
 
   nix.settings = {
     # Enable flakes and new 'nix' command
