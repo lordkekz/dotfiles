4629b39637892a6f218cb117b7a069b67eb52d39
lordkekz <lordkekz@lkekz.de>
2024-05-25 20:41:08 +0200

    homeProfiles/graphical: Enable firefox profile desktopfile on Hyprland

--- homeProfiles/graphical/firefox.nix
+++ homeProfiles/graphical/firefox.nix
@@ -127,7 +127,8 @@ in {
         settings-default
         // {};
       inherit search;
-      desktopFile.enable = false;
+      # Generate the entry only for Hyprland
+      desktopFile.enable = config.wayland.windowManager.hyprland.enable;
       desktopFile.icon = "${inputs.self.outPath}/assets/Firefox_Homework_Icon_128x128.png";
     };
   };
