4d314b88a989375a27fecf9d3f310580ff7175a1
lordkekz <lordkekz@lkekz.de>
2024-06-22 15:18:37 +0200

    hypridle: Fix screen lock, don't off screen, notify before lock

--- homeProfiles/hypr/hypridle.nix
+++ homeProfiles/hypr/hypridle.nix
@@ -13,26 +13,33 @@ args @ {
   ...
 }: let
   hyprlock = "${config.programs.hyprlock.package}/bin/hyprlock";
+  dunstify = "${config.services.dunst.package}/bin/dunstify";
 in {
   services.hypridle = {
     enable = true;
     settings = {
       general = {
-        after_sleep_cmd = "hyprctl dispatch dpms on";
         ignore_dbus_inhibit = false;
         lock_cmd = "pidof hyprlock || ${hyprlock}";
+        unlock_cmd = "pkill -SIGUSR1 hyprlock";
+        before_sleep_cmd = "loginctl lock-session";
+        after_sleep_cmd = "hyprctl dispatch dpms on";
       };
 
       listener = [
         {
+          timeout = 290; # seconds
+          on-timeout = ''${dunstify} -t 10000 -a hypridle "About to lock screen..."'';
+        }
+        {
           timeout = 300; # seconds
           on-timeout = "loginctl lock-session";
         }
-        {
-          timeout = 600;
-          on-timeout = "hyprctl dispatch dpms off";
-          on-resume = "hyprctl dispatch dpms on";
-        }
+        #{
+        #  timeout = 600;
+        #  on-timeout = "hyprctl dispatch dpms off";
+        #  on-resume = "hyprctl dispatch dpms on";
+        #}
       ];
     };
   };
