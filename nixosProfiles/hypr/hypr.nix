57d8cd6ba088b6971e992062e8b24634fa882b4e
lordkekz <lordkekz@lkekz.de>
2024-07-03 17:45:04 +0200

    Hyprland: Increase performance with TLP on AC

--- nixosProfiles/hypr/hypr.nix
+++ nixosProfiles/hypr/hypr.nix
@@ -23,12 +23,12 @@ in {
     settings = {
       CPU_DRIVER_OPMODE_ON_AC = "active";
       CPU_DRIVER_OPMODE_ON_BAT = "passive";
-      CPU_SCALING_GOVERNOR_ON_AC = "powersave";
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-      CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
       CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
       CPU_MIN_PERF_ON_AC = 0;
-      CPU_MAX_PERF_ON_AC = 80;
+      CPU_MAX_PERF_ON_AC = 100;
       CPU_MIN_PERF_ON_BAT = 0;
       CPU_MAX_PERF_ON_BAT = 15;
       CPU_BOOST_ON_AC = 1;
