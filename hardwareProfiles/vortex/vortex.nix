34669af33fb65799f1b247e3d183b179eb734c91
lordkekz <lordkekz@lkekz.de>
2024-10-17 01:44:32 +0200

    vortex: Use systemd-boot

--- hardwareProfiles/vortex/vortex.nix
+++ hardwareProfiles/vortex/vortex.nix
@@ -8,7 +8,18 @@
   modulesPath,
   ...
 }: {
-  imports = [hardwareProfiles.common];
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+    (modulesPath + "/profiles/qemu-guest.nix")
+    hardwareProfiles.common
+  ];
+
+  boot.loader.grub = {
+    # no need to set devices, disko will add all devices that have a EF02 partition to the list already
+    # devices = [ ];
+    efiSupport = true;
+    efiInstallAsRemovable = true;
+  };
 
   networking.hostId = "f5bf3143";
   networking.useNetworkd = true;
