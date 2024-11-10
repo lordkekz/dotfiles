93ec199d64b06fbadfcae6a8b4ed9e74bfab5f62
lordkekz <lordkekz@lkekz.de>
2024-11-10 23:43:18 +0100

    nasman2404: Generate TLS certificates with ACME

---
+++ hardwareProfiles/ryzen-server-2404/acme.nix
@@ -0,0 +1,29 @@
+# Generate ACME certs for my domains
+{
+  inputs,
+  outputs,
+  nixosProfiles,
+  lib,
+  config,
+  pkgs,
+  ...
+}: let
+  cloudflare = domain: {
+    domain = "*.${domain}";
+    dnsProvider = "cloudflare";
+    # location of your CLOUDFLARE_DNS_API_TOKEN=[value]
+    # https://www.freedesktop.org/software/systemd/man/latest/systemd.exec.html#EnvironmentFile=
+    environmentFile = config.age.secrets.cloudflare-token.path;
+  };
+in {
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = "info@lkekz.de";
+    certs = {
+      "hepr.me" = cloudflare "hepr.me";
+      "solux.cc" = cloudflare "solux.cc";
+    };
+  };
+
+  age.secrets.cloudflare-token.rekeyFile = "${inputs.self.outPath}/secrets/cloudflare-token.age";
+}
