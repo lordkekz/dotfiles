3f8f5def7b45a094f9a5842fbe9c86694634c10a
lordkekz <lordkekz@lkekz.de>
2024-11-27 01:35:16 +0100

    vortex: Add public websites

---
+++ workloadProfiles/public-websites.nix
@@ -0,0 +1,36 @@
+{
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  personal-data,
+  ...
+}: {
+  services.caddy.virtualHosts."hepr.me" = {
+    serverAliases = ["www.hepr.me"];
+    extraConfig = ''
+      tls /var/lib/acme/hepr.me/cert.pem /var/lib/acme/hepr.me/key.pem
+        header Content-Type text/html
+        respond <<HTML
+          <html>
+            <head><title>hepr.me</title></head>
+            <body>Site under construction.</body>
+          </html>
+          HTML 200
+    '';
+  };
+  services.caddy.virtualHosts."r4c.hepr.me" = {
+    serverAliases = ["www.r4c.hepr.me"];
+    extraConfig = ''
+      tls /var/lib/acme/hepr.me/cert.pem /var/lib/acme/hepr.me/key.pem
+        header Content-Type text/html
+        respond <<HTML
+          <html>
+            <head><title>Robot4Care</title></head>
+            <body>November Demo is not set up yet.</body>
+          </html>
+          HTML 200
+    '';
+  };
+}
