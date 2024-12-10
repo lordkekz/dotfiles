bd4e77dbe519a370aacef48825a32f36572b272d
lordkekz <lordkekz@lkekz.de>
2024-12-10 01:13:03 +0100

    workloads: Increase memory and vcpu count

--- workloadProfiles/__microvmBaseConfig.nix
+++ workloadProfiles/__microvmBaseConfig.nix
@@ -9,6 +9,9 @@
   pkgs,
   ...
 }: {
+  microvm.mem = 3072; # This can't be exactly 2GiB because QEMU hangs otherwise
+  microvm.vcpu = 4;
+
   systemd.network.enable = true;
   microvm.interfaces = [
     {
