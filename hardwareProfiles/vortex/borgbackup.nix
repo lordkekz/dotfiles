7fe63b40f6daf972090b1412eaf4d88ead700a8a
lordkekz <lordkekz@lkekz.de>
2025-01-06 01:34:17 +0100

    vortex: Add passphrase for borg encryption

--- hardwareProfiles/vortex/borgbackup.nix
+++ hardwareProfiles/vortex/borgbackup.nix
@@ -12,13 +12,18 @@
       ];
       doInit = true;
       repo = "borg@nasman2404:/orion/backups/borg";
+      encryption = {
+        mode = "repokey";
+        passCommand = "cat ${config.age.secrets.borgbackup-passphrase-orion.path}";
+      };
       environment.BORG_RSH = "ssh -i ${config.age.secrets.borgbackup-key-vortex.path}";
       compression = "auto,lzma";
       startAt = "daily";
     };
   };
 
-  age.secrets.borgbackup-key-vortex = {
-    rekeyFile = "${inputs.self.outPath}/secrets/borgbackup-key-vortex.age";
+  age.secrets = {
+    borgbackup-key-vortex.rekeyFile = "${inputs.self.outPath}/secrets/borgbackup-key-vortex.age";
+    borgbackup-passphrase-orion.rekeyFile = "${inputs.self.outPath}/secrets/borgbackup-passphrase-orion.age";
   };
 }
