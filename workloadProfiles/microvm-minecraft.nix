c65143540106d4e2384ad34ef2f6bfa312329293
lordkekz <lordkekz@lkekz.de>
2025-01-07 02:40:06 +0100

    minecraft: Add BlueMap

--- workloadProfiles/microvm-minecraft.nix
+++ workloadProfiles/microvm-minecraft.nix
@@ -10,7 +10,6 @@
   vmName = "minecraft";
   vmId = "16";
   domain = "mc.hepr.me";
-  internalIP = "10.0.0.13";
   user = "minecraft";
   group = "minecraft";
   unitsAfterPersist = ["minecraft-server-survival2.service"];
@@ -25,7 +24,7 @@ in {
     request_body {
       max_size 1GB
     }
-    reverse_proxy http://${internalIP}:8000
+    reverse_proxy http://10.0.0.${vmId}:8100
   '';
 
   networking.firewall.allowedTCPPorts = [25565];
@@ -44,10 +43,11 @@ in {
       inputs.nix-minecraft.nixosModules.minecraft-servers
     ];
 
-    microvm.mem = lib.mkForce 3584; # MiB
+    microvm.vcpu = lib.mkForce 10;
+    microvm.mem = lib.mkForce 6144; # MiB
 
     networking.firewall.interfaces = {
-      "vm-${vmName}-a".allowedTCPPorts = [25565];
+      "vm-${vmName}-a".allowedTCPPorts = [8100 25565];
       "vm-${vmName}-b".allowedTCPPorts = [];
     };
 
@@ -62,7 +62,6 @@ in {
         restart = "always"; # TODO this may be a default
         jvmOpts = "-Xmx3G";
         package = paperServer;
-        symlinks."cache/mojang_${minecraftVersion}.jar" = paperServer.vanillaJar;
         serverProperties = {
           motd = "Minecraft Survival 2";
           server-port = 25565;
@@ -74,6 +73,33 @@ in {
         whitelist = {
           "LordKekz" = "e98caca4-c4d6-42d2-88f0-d37dd965d365";
           "UrsulaUnke" = "b5bee075-5a0d-4c63-9c8f-9c5c230f0da3";
+        };
+        symlinks = {
+          # This avoids runtime downloads of server jar
+          "cache/mojang_${minecraftVersion}.jar" = paperServer.vanillaJar;
+
+          # BlueMap plugin generates a 3D browser map of minecraft worlds
+          "plugins/bluemap.jar" = pkgs.fetchurl {
+            url = "https://github.com/BlueMap-Minecraft/BlueMap/releases/download/v5.5/bluemap-5.5-paper.jar";
+            hash = "sha256-nZxBbF1KkGHveZCKPJ0hHyJGXHnNSCKTvX5JRr0+s88=";
+          };
+
+          # Link BlueMap configs from this repo
+          "plugins/BlueMap/maps" = "${inputs.self.outPath}/assets/minecraft-bluemap-default-configs/maps";
+          "plugins/BlueMap/storages" = "${inputs.self.outPath}/assets/minecraft-bluemap-default-configs/storages";
+          "plugins/BlueMap/core.conf" = "${inputs.self.outPath}/assets/minecraft-bluemap-default-configs/core.conf";
+          "plugins/BlueMap/webapp.conf" = "${inputs.self.outPath}/assets/minecraft-bluemap-default-configs/webapp.conf";
+          "plugins/BlueMap/webserver.conf" = "${inputs.self.outPath}/assets/minecraft-bluemap-default-configs/webserver.conf";
+        };
+        files = {
+          # Set myself as an Operator
+          "ops.json".value = [
+            {
+              name = "LordKekz";
+              uuid = "e98caca4-c4d6-42d2-88f0-d37dd965d365";
+              level = 4;
+            }
+          ];
         };
       };
     };
