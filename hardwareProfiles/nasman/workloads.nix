c6cd8335a6f8968609a857cf6037cd3618c8c68f
lordkekz <lordkekz@lkekz.de>
2025-03-20 21:52:30 +0100

    workloads: Remove robots stuff

--- hardwareProfiles/nasman/workloads.nix
+++ hardwareProfiles/nasman/workloads.nix
@@ -20,7 +20,6 @@ in {
     microvm-forgejo
     microvm-minecraft
     frigate-cctv
-    robots
     ollama
   ];
 
--- workloadProfiles/public-websites.nix
+++ workloadProfiles/public-websites.nix
@@ -49,39 +49,6 @@
     '';
   };
 
-  services.caddy.virtualHosts."r4c.hepr.me" = {
-    serverAliases = ["www.r4c.hepr.me"];
-    extraConfig = ''
-      tls /var/lib/acme/r4c.hepr.me/cert.pem /var/lib/acme/r4c.hepr.me/key.pem
-      redir luna.r4c.hepr.me{uri}
-    '';
-  };
-  services.caddy.virtualHosts."luna.r4c.hepr.me".extraConfig = ''
-    tls /var/lib/acme/r4c.hepr.me/cert.pem /var/lib/acme/r4c.hepr.me/key.pem
-    reverse_proxy http://localhost:8001 {
-      header_down X-Real-IP {http.request.remote}
-      header_down X-Forwarded-For {http.request.remote}
-    }
-  '';
-  services.caddy.virtualHosts."luna.r4c.hepr.me:9090".extraConfig = ''
-    tls /var/lib/acme/r4c.hepr.me/cert.pem /var/lib/acme/r4c.hepr.me/key.pem
-    reverse_proxy http://localhost:9001
-  '';
-
-  services.caddy.virtualHosts."robi.r4c.hepr.me".extraConfig = ''
-    tls /var/lib/acme/r4c.hepr.me/cert.pem /var/lib/acme/r4c.hepr.me/key.pem
-    reverse_proxy http://localhost:8002 {
-      header_down X-Real-IP {http.request.remote}
-      header_down X-Forwarded-For {http.request.remote}
-    }
-  '';
-  services.caddy.virtualHosts."robi.r4c.hepr.me:9090".extraConfig = ''
-    tls /var/lib/acme/r4c.hepr.me/cert.pem /var/lib/acme/r4c.hepr.me/key.pem
-    reverse_proxy http://localhost:9002
-  '';
-
-  networking.firewall.allowedTCPPorts = [9090];
-
   # Enable podman containers
   virtualisation.podman = {
     enable = true;
