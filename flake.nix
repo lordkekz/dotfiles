f1fd5ed4dff790e66a16382bcaecbb9fb957763c
lordkekz <lordkekz@lkekz.de>
2025-04-26 00:11:12 +0200

    flake, modules: Use nixpkgs-stable from fork, drop syncthing module

--- flake.nix
+++ flake.nix
@@ -83,7 +83,8 @@
     naersk.inputs.nixpkgs.follows = "nixpkgs";
 
     # Nixpkgs
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
+    # nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs-stable.url = "github:lordkekz/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs.follows = "nixpkgs-stable";
 
