75d38ad4c04f1b7ab0fd6f56747ca650d5159f75
lordkekz <lordkekz@lkekz.de>
2025-06-12 23:34:45 +0200

    hardwareProfiles: Enable periodic filesystem scrubs

--- hardwareProfiles/common/common.nix
+++ hardwareProfiles/common/common.nix
@@ -6,4 +6,29 @@
   pkgs,
   modulesPath,
   ...
-}: {}
+}: let
+  hasFS = fsType:
+    with lib;
+      pipe config.fileSystems [
+        attrValues
+        (catAttrs "fsType")
+        (any (x: x == fsType))
+      ];
+  hasZFS = hasFS "zfs";
+  hasBTRFS = hasFS "btrfs";
+in {
+  services.zfs = {
+    trim = {
+      enable = lib.mkDefault hasZFS;
+      interval = "weekly";
+    };
+    autoScrub = {
+      enable = lib.mkDefault hasZFS;
+      interval = "weekly";
+    };
+  };
+  services.btrfs.autoScrub = {
+    enable = lib.mkDefault hasBTRFS;
+    interval = "weekly";
+  };
+}
